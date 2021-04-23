cfb3b1c1026ead0f33613c5bd14c121060764455
Giulia-M <giuliamaggio16@gmail.com>
2021-04-23 13:49:05 +0200

    integrato con il suggerimento di Florian

--- myScript.js
+++ myScript.js
@@ -43,14 +43,15 @@ var arrayListaStudenti = [
 // Ciclare su tutti gli studenti (che è un array, cioè arrayListaStudenti) e stampare per ognuno di essi, nome e cognome.
 for (var i = 0; i < arrayListaStudenti.length; i++) {
     //con element recupero il mio primo oggetto nell'array "arrayListaStudenti"
-    var element = arrayListaStudenti[i]; 
-  
+    var element = arrayListaStudenti[i];
+
     //stampo nome e cognome di tutti gli studenti perchè sono nel ciclo for 
     console.log(element.nome)
     console.log(element.cognome)
 
 }
 
+/*
 // attraverso 3 prompt() aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età
 var inputNome = prompt("inserisci il tuo nome");
 var inputCognome = prompt("Inserisci il tuo cognome");
@@ -71,3 +72,54 @@ console.log(arrayListaStudenti[3].nome)
 console.log(arrayListaStudenti[3].cognome)
 console.log(arrayListaStudenti[3].eta)
 
+
+*/
+
+//soluzione Florian
+var mappaRichieste = [
+    {
+        domanda: "inserisci il nome dello studente",
+        chiave: "nome"
+    },
+    {
+        domanda: "inserisci il cognome dello studente",
+        chiave: "cognome"
+    },
+    {
+        domanda: "inserisci l'età dello studente",
+        chiave: "eta",
+        validation: "numero"
+    }
+]
+
+
+var nuovoStudente = {}
+
+for (var i = 0; i < mappaRichieste.length; i++) {
+    var oggMappa = mappaRichieste[i];
+    var risultatoPrompt = prompt(oggMappa.domanda);
+    var risultatoValidazione = true;
+
+    if (oggMappa.validation === "numero") {
+        risultatoPrompt = parseInt(risultatoPrompt);
+
+        if (isNaN(risultatoPrompt)) {
+            i--;
+            risultatoValidazione = false;
+
+            alert("L'età inserita non è valida");
+        }
+    }
+
+    if (risultatoValidazione) {
+        nuovoStudente[oggMappa.chiave] = risultatoPrompt;
+    }
+}
+
+
+
+
+
+
+arrayListaStudenti.push(nuovoStudente)
+console.log(arrayListaStudenti)
